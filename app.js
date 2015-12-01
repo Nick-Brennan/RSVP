6b6d88243ea75e2542a5db9bd108c691f7279977
Nick Brennan <nick_p_b@yahoo.com>
2015-12-01 13:19:01 -0800

    more angular

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 if(Meteor.isClient){
-	angular.module('rsvp', ['angular-meteor']);
+	var app = angular.module('rsvp', ['angular-meteor']);
+
+	app.controller('PartiesListCtrl', ['$scope', function($scope){
+		$scope.parties = [
+			{
+				'name': 'No EDM Party',
+				'description': 'There will be no EDM at this party'
+			},
+			{
+				'name': 'All EDM, All the time',
+				'description': 'Bring it!'
+			},
+			{
+				'name': 'Chill Zone',
+				'description': 'Mellow'
+			}
+		];
+	}]);
 }
