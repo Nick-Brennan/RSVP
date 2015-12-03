93cd1ed9a8f1b8853171065061ade121cc2fb950
Nick Brennan <nick_p_b@yahoo.com>
2015-12-02 17:13:18 -0800

    implemented delete events

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ if(Meteor.isClient){
 
 	app.controller('PartiesListCtrl', ['$scope', '$meteor', function($scope, $meteor){
 		$scope.parties = $meteor.collection(Parties);
+
+		$scope.remove = function(party){
+			$scope.parties.splice($scope.parties.indexOf(party), 1);
+		};
 	}]);
 }
