7504193107dd71484d4c83b19047ebc3de4115b6
Nick Brennan <nick_p_b@yahoo.com>
2015-12-02 17:07:29 -0800

    implemented event creation fully data bound from db to view

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
+
 if(Meteor.isClient){
 	var app = angular.module('rsvp', ['angular-meteor']);
 
-	app.controller('PartiesListCtrl', ['$scope', function($scope){
-		$scope.parties = [
-			{
-				'name': 'No EDM Party',
-				'description': 'There will be no EDM at this party'
-			},
-			{
-				'name': 'All EDM, All the time',
-				'description': 'Bring it!'
-			},
-			{
-				'name': 'Chill Zone',
-				'description': 'Mellow'
-			}
-		];
+	app.controller('PartiesListCtrl', ['$scope', '$meteor', function($scope, $meteor){
+		$scope.parties = $meteor.collection(Parties);
 	}]);
 }
