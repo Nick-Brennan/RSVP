32f0518b64c4c9cc172afb822b21a7b34ffdcef9
Nick Brennan <nick_p_b@yahoo.com>
2015-12-04 13:25:28 -0800

    added event details view and routing

--- app.js
+++ app.js
@@ -2,6 +2,24 @@
 if(Meteor.isClient){
 	var app = angular.module('rsvp', ['angular-meteor', 'ui.router']);
 
+	app.config(function($urlRouterProvider, $stateProvider, $locationProvider){
+		$locationProvider.html5Mode(true);
+
+		$stateProvider
+			.state('events',{
+				url: '/events',
+				templateUrl: 'event-list.html',
+				controller: 'PartiesListCtrl'
+			})
+			.state('eventDetails', {
+				url: '/events/:partyId',
+				templateUrl: 'event-details.html',
+				controller: 'EventDetailsCtrl'
+			});
+
+		$urlRouterProvider.otherwise("/events");	
+	});
+
 	app.controller('PartiesListCtrl', ['$scope', '$meteor', function($scope, $meteor){
 		$scope.parties = $meteor.collection(Parties);
 
@@ -12,5 +30,9 @@ if(Meteor.isClient){
 		$scope.removeAll = function(){
 			$scope.parties.remove();
 		}
+	}]);
+
+	app.controller('EventDetailsCtrl', ['$scope', '$stateParams', function($scope, $stateParams){
+		$scope.partyId = $stateParams.partyId;
 	}]);
 }
