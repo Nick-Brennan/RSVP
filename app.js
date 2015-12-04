5eacb457f89df4fd8b8da828f7dd2e06542c831e
Nick Brennan <nick_p_b@yahoo.com>
2015-12-04 12:52:41 -0800

    added angular ui-router module

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 if(Meteor.isClient){
-	var app = angular.module('rsvp', ['angular-meteor']);
+	var app = angular.module('rsvp', ['angular-meteor', 'ui.router']);
 
 	app.controller('PartiesListCtrl', ['$scope', '$meteor', function($scope, $meteor){
 		$scope.parties = $meteor.collection(Parties);
